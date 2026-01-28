1baa5e4b075985a869d065ca11a8105021b2e2cd
Nikketryhard <louisnewmobile@gmail.com>
2026-01-27 21:02:46 -0600

    chore: add tool barrel export

---
+++ src/tools/index.ts
@@ -0,0 +1,6 @@
+export { handleStart, startInputSchema, type StartInput, type StartResult } from "./start";
+export { handlePropose, proposeInputSchema, type ProposeInput, type ProposeResult } from "./propose";
+export { handleCommit, commitInputSchema, type CommitInput, type CommitResult } from "./commit";
+export { handleReclassify, reclassifyInputSchema, type ReclassifyInput, type ReclassifyResult } from "./reclassify";
+export { handleStatus, statusInputSchema, type StatusInput, type StatusResult } from "./status";
+export { handleEnd, endInputSchema, type EndInput, type EndResult } from "./end";
